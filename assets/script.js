d37679e586fa0afe4269af62018ab3598c5d2572
Osmilliardo <140178301+Osmilliardo@users.noreply.github.com>
2023-09-17 14:19:26 -0400

    added some button functionality for the start button, expanded the layout for the question and answer sections of the html

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+// var q1Card = $('#questionCard1')
+// var q2Card = $('#questionCard2')
+// var q3Card = $('#questionCard3')
+// var q4Card = $('#questionCard4')
+// var q5Card = $('#questionCard5')
+
+var timeLeft = 120
+var score = 100
+
+$('#startBtn').on('click', function () {
+    if ($('#questionCard1').hasClass('hidden')) {
+        console.log("it is hidden")
+        
+    } else console.log('something broke')
+
+})
